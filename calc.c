3df4beeacc124c1f5b674262e6eb59ff0ecca537
Alexander Iansiti <aiansiti@outlook.com>
2017-05-21 00:24:53 -0400

    began rewritting existing code to work more like the Python spec

--- calc.c
+++ calc.c
@@ -3,96 +3,63 @@
 #include <string.h>
 #include <stdlib.h>
 
+
 typedef struct Token {
 	int value;
-  char type[];
+  char type[10];
 } Token;
 
-int int_plus_int(Token *seq[], int length){
-  if(length>3){
-    return 0;
-  }
-  if(strcmp(seq[0]->type, "INTEGER")!=0){
-    printf("%s\n", "error1");
-    return 0;
-  }
-  if(strcmp(seq[1]->type, "PLUS")!=0){
-    printf("%s\n", "error2");
+int current_pos = 0;
+char current_char;
+Token current_token;
+int length_of_input;
 
-    return 0;
+Token next_token(){
+  while (current_char != NULL) {
+    if(input[i] == ' '){
+      continue;
+    }
+    if(isdigit(current_char)){
+      printf("%s\n", "INTEGER");
+      Token *current = malloc(sizeof(Token));
+      strncpy(current->type, "INTEGER", sizeof("INTEGER"));
+      current->value = current_char - '0';
+      return current;
+    }
+    if(current_char == '+'){
+      printf("%s\n", "PLUS");
+      Token *current = malloc(sizeof(Token));
+      strncpy(current->type, "PLUS", sizeof("PLUS"));
+      return current;
+    }
+    if(current_char == '-'){
+      printf("%s\n", "MINUS");
+      Token *current = malloc(sizeof(Token));
+      strncpy(current->type, "MINUS", sizeof("MINUS"));
+      return current;
+    }
   }
-  if(strcmp(seq[2]->type, "INTEGER")!=0){
-    printf("%s\n", "error3");
-
-    return 0;
-  }
-
-  return 1;
 }
-
-int int_minus_int(Token *seq[], int length){
-  if(length>3){
-    return 0;
+void consume(char type[]){
+  if(strcmp(current_token.type, type)==0){
+    current_token = next_token();
   }
-  if(strcmp(seq[0]->type, "INTEGER")!=0){
-    printf("%s\n", "error1");
-    return 0;
+  else{
+    printf("Wrong Token Type Expected\n");
   }
-  if(strcmp(seq[1]->type, "MINUS")!=0){
-    printf("%s\n", "error2");
-
-    return 0;
+}
+void advance(char input[]){
+  current_pos++;
+  if(current_pos>length_of_input){
+    printf("End of Sequence\n");
+    return;
   }
-  if(strcmp(seq[2]->type, "INTEGER")!=0){
-    printf("%s\n", "error3");
-
-    return 0;
-  }
-  return 1;
-
+  current_char = input[current_pos];
 }
 
 int main(int argc, char *argv[])
 {
   char input[10];
   strncpy(input, argv[1], sizeof(argv[1]));
-  Token *sequence[strlen(input)];
-  int seq_length = 0;
-  for(int i = 0; i<strlen(input); i++){
-    if(input[i] == ' '){
-      continue;
-    }
-    if(isdigit(input[i])){
-      printf("%s\n", "INTEGER");
-      seq_length++;
-      Token *current = malloc(sizeof(Token));
-      strncpy(current->type, "INTEGER", sizeof("INTEGER"));
-      current->value = input[i] - '0';
-      sequence[i] = current;
-    }
-    if(input[i] == '+'){
-      printf("%s\n", "PLUS");
-      seq_length++;
-      Token *current = malloc(sizeof(Token));
-      strncpy(current->type, "PLUS", sizeof("PLUS"));
-      sequence[i] = current;
-    }
-    if(input[i] == '-'){
-      printf("%s\n", "MINUS");
-      seq_length++;
-      Token *current = malloc(sizeof(Token));
-      strncpy(current->type, "MINUS", sizeof("MINUS"));
-      sequence[i] = current;
-    }
-  }
-  if(int_plus_int(sequence, seq_length)==1){
-    printf("%d\n", sequence[0]->value + sequence[2]->value);
-    return 0;
-  }
-  if(int_minus_int(sequence, seq_length)==1){
-    printf("%d\n", sequence[0]->value - sequence[2]->value);
-    return 0;
-  }
-  printf("%s\n", "error parsing input");
-  return 0;
+  length_of_input = sizeof(input)/sizeof(input[0]);
 }
